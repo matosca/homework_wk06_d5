6a633874fd9e53a782486839bb2bf91966e6a6ef
marisol <mtoscanor37@gmail.com>
2019-04-06 00:35:22 +0100

    Fixed clear list button

--- js/app.js
+++ js/app.js
@@ -2,5 +2,37 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
   //form
-  // const form = document.querySelector('#task-form');
+  const form = document.querySelector('#task-form');
+  const handleFormAdd = function (event) {
+    event.preventDefault();
+
+    const resultList = document.querySelector('#table');
+
+    const tableListTasks = document.querySelector('#task-list');
+
+    const listItems = document.createElement('td');
+    listItems.classList.add('tasks');
+
+    listItems.textContent = `${event.target[0].value} ${event.target[1].value} ${event.target[2].value} ${event.target[3].value}`;
+
+    document.getElementById('task-form').reset();
+
+    tableListTasks.appendChild(listItems);
+  };
+
+  form.addEventListener('submit', handleFormAdd);
+
+  const clearForm = document.querySelector('#delete');
+    const handleFormDelete = (event) => {
+      event.preventDefault();
+
+      const resultList = document.querySelector('#task-list');
+      const children = document.querySelectorAll('.tasks');
+
+      children.forEach((child) => {
+        resultList.removeChild(child);
+      });
+    };
+
+    clearForm.addEventListener('submit', handleFormDelete);
 });
