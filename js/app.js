7ffc0852265f7ccb33cdf63a6495bfbce20e8d90
marisol <mtoscanor37@gmail.com>
2019-04-07 11:58:03 +0100

    Added style to form

--- js/app.js
+++ js/app.js
@@ -6,18 +6,45 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormAdd = function (event) {
     event.preventDefault();
 
-    const resultList = document.querySelector('#table');
+    //Selected #content assigned to variable result
+    const resultTaskList = document.querySelector('#content');
 
-    const tableListTasks = document.querySelector('#task-list');
+    //Const capturing the event target values
+    const taskName = event.target[0].value;
+    const taskDescription = event.target[1].value;
+    const taskDueDate = event.target[2].value;
+    const taskTagSelect = event.target[3].value;
 
-    const listItems = document.createElement('td');
-    listItems.classList.add('tasks');
+    //Main Container for resulted list created
+    // const mainContent = document.createElement('div');
+    // mainContent.classList.add('task-list');
 
-    listItems.textContent = `${event.target[0].value} ${event.target[1].value} ${event.target[2].value} ${event.target[3].value}`;
+    // list items captures in const to append to resultTaskList
+    const listItemName = document.createElement('h5');
+    listItemName.textContent = taskName;
+    listItemName.classList.add('heading');
 
+    const listItemDescription = document.createElement('p');
+    listItemDescription.textContent = taskDescription;
+    listItemDescription.classList.add('description');
+
+    const listItemDate = document.createElement('p');
+    listItemDate.textContent = `Due Date: ${taskDueDate} | ${taskTagSelect}`;
+    listItemDate.classList.add('date');
+
+    // const listItemTag = document.createElement('span');
+    // listItemTag.textContent = taskTagSelect;
+    // listItemTag.classList.add('tag');
+
+    resultTaskList.appendChild(listItemName);
+    resultTaskList.appendChild(listItemDescription);
+    resultTaskList.appendChild(listItemDate);
+
+    // listItems.textContent = `${event.target[0].value} ${event.target[1].value} ${event.target[2].value} ${event.target[3].value}`;
+    // resultTaskList.appendChild(listItems);
+
+    //Reseting the form
     document.getElementById('task-form').reset();
-
-    tableListTasks.appendChild(listItems);
   };
 
   form.addEventListener('submit', handleFormAdd);
@@ -26,8 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const handleFormDelete = (event) => {
       event.preventDefault();
 
-      const resultList = document.querySelector('#task-list');
-      const children = document.querySelectorAll('.tasks');
+      const resultList = document.querySelector('#content');
+      const children = document.querySelectorAll('.heading, .description, .date');
 
       children.forEach((child) => {
         resultList.removeChild(child);
