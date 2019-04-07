f27a329214edbe33cc1c8eb729461a44db49bc14
marisol <mtoscanor37@gmail.com>
2019-04-07 18:19:31 +0100

    Finished styling CSS web app

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskTagSelect = event.target[3].value;
 
     // list items captures in const to append to resultTaskList
-    const listItemName = document.createElement('h5');
+    const listItemName = document.createElement('h3');
     listItemName.textContent = taskName;
     listItemName.classList.add('heading');
 
@@ -25,12 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
     listItemDescription.classList.add('description');
 
     const listItemDate = document.createElement('p');
-    // listItemDate.textContent = `Due Date: ${taskDueDate} | ${taskTagSelect}`;
+    listItemDate.textContent = `Due Date: ${taskDueDate} | ${taskTagSelect}`;
     listItemDate.classList.add('date');
-
-    const listItemTag = document.createElement('span');
-    listItemTag.textContent = taskTagSelect;
-    listItemTag.classList.add('tag');
 
     resultTaskList.appendChild(listItemName);
     resultTaskList.appendChild(listItemDescription);
@@ -56,3 +52,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     clearForm.addEventListener('submit', handleFormDelete);
 });
+//   //Select dropdown
+//   const selectDropdown = document.querySelector('#tag-selector');
+//
+//   const handleSelect = function(event) {
+//     const resultSelected = event.target.value;
+//     function changeColor(event) {
+//       let blue = document.getElementById('blue');
+//       let green = document.getElementById('green');
+//       let orange = document.getElementById('orange');
+//       let purple = document.getElementById('purple');
+//
+//       if(event.target.value === blue){
+//         blue.style.color = "blue";
+//       } else if (event.target.value === green) {
+//         green.style.color = "green";
+//       } else if (event.target.value === orange) {
+//         orange.style.color = "orange";
+//       } else if (event.target.value === purple){
+//         purple.style.color = "purple";
+//       } else {
+//         alert("There was an error");
+//       }
+//     };
+//     handleSelect.changeColor()
+//     selectDropdown.addEventListener('change', handleSelect);
